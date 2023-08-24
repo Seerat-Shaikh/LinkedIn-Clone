081ae6da024f818d0120b85641b6750de838cf52
Seerat Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-08-24 23:40:36 +0500

    Reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,9 +1,16 @@
+import { SET_USER } from "../actions/actionType";
+
 const INITIAL_STATE = {
     user: null,
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) { //like post and increment it by 1
+        case SET_USER:
+            return {
+                ...state,
+                user: action.user,
+            };
         default:
             return state; //update te new state otherwise return the existing actual state 
     }
